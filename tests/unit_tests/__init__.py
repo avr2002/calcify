2eb562b4572adcff5d3029766d4a6e4717be7a6e
Amit Vikram Raj <82011740+avr2002@users.noreply.github.com>
2024-06-11 16:08:38 +0000

    feat: populated the repository from the `python-cookiecutter-template`

---
+++ tests/unit_tests/__init__.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for calcify.
+
+This folder ideally has a parallel folder structure with the
+`src/calcify/` folder.
+
+In general, unit tests
+
+- should be fast and test a single function or class
+- should not depend on external resources (e.g., databases, network, etc.).
+"""
